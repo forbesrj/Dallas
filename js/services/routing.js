d0a3365e9d5c300ba2d08e317938f310dbdaff48
forbesrj <forbesrj@gmail.com>
2014-03-22 22:18:28 -0500

    Styles updated

--- js/services/routing.js
+++ js/services/routing.js
@@ -5,6 +5,7 @@ var app = angular.module('menuModule', ['menuServices']);
 
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider
-        .when('/menu/:id', {templateUrl:'partials/item.html', controller: 'ItemCtrl'})
+        .when('/item/:id', {templateUrl:'partials/item.html', controller: 'ItemCtrl'})
+        .when('/about', {templateUrl:'partials/about.html', controller: ''})
         .otherwise({redirectTo: '/menu', templateUrl:'partials/menu.html'})
 }]);
